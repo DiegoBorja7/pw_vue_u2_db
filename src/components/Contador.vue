f50802553ff6dc64b1f0b494f37bff1a4166c661
DiegoBorja7 <daborjas@uce.edu.ec>
2025-11-21 19:45:48 -0500

    Componentes Vue.js

---
+++ src/components/Contador.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Universidad Central del Ecuador</h1>
+        <h2>Carrera de computación</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Contador'
+}
+</script>
+
+<style>
+</style>
---
+++ src/view/ContadorView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Contador/>
+    </div>
+</template>
+
+<script>
+import Contador from '../components/Contador.vue';
+
+export default {
+  name: 'ContadorView',
+  components: {
+    Contador
+  }
+}
+</script>
+
+<style>
+</style>
